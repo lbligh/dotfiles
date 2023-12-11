a69138d074ec975a2c5183b52d8c1ca5a1f856f6
Luicus Bligh <lucius@lucius.org.uk>
2023-12-11 16:14:18 +0000

    Done and Tested

--- brew.sh
+++ brew.sh
@@ -1,17 +1,48 @@
 #!/usr/bin/env bash
 
 # Install Brew Packages
-brew install python
-brew install tree
 
-# Install MacOS Applications
-brew cask install google-chrome
-brew cask install virtualbox
-brew cask install spotify
-brew cask install discord
-brew cask install gimp
+brew install python
+
+echo "Installing other brew stuff..."
+brew install tree
+brew install wget
+brew install trash
+brew install svn
+brew install mackup
+brew install node
+
+echo "Cleaning up brew"
+brew cleanup
+
+echo "Installing homebrew cask"
+brew install caskroom/cask/brew-cask
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
 brew cask install font-source-code-pro
 
+ps=(
+  alfred
+  bartender
+  dropbox
+  google-chrome
+  spotify
+  iterm2
+  virtualbox
+  transmission
+  zoomus
+  onepassword
+  discord
+  gimp
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps with Cask..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+brew cask alfred link
+
+brew cask cleanup
+brew cleanup
